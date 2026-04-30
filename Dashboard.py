095af756551017ae61cd5e447be601f3728ccdcd
Julio Alberto Ramírez Vera <julioalbertoramirezvera@gmail.com>
2026-04-30 18:54:08 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -24,7 +24,7 @@ st.set_page_config(
     layout="wide",
     page_icon="📊"
 )
-st.title("📊 Sistema econométrico y Business Intelligence para la gestión estratégica del crecimiento económico")
+st.title("📊 Econometría con Business Intelligence para la gestión estratégica del crecimiento económico")
 st.markdown("Fuente: Banco Mundial  \
 Proyección: ARIMA  \
 Gráficos interactivos Plotly")
